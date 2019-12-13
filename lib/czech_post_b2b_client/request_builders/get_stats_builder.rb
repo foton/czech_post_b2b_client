0cafcfbf273cbc0ee691fe22cd40f2d402911bf7
Petr Mlčoch <foton@centrum.cz>
2019-12-13 14:36:57 +0100

    building XML with Ox gem

--- lib/czech_post_b2b_client/request_builders/get_stats_builder.rb
+++ lib/czech_post_b2b_client/request_builders/get_stats_builder.rb
@@ -9,22 +9,51 @@ module CzechPostB2bClient
       end
 
       def to_xml
-        <<~XML
-          <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-          <b2bRequest #{namespaces}>
-            <header>
-              <idExtTransaction>1</idExtTransaction>
-              <timeStamp>#{Time.now.strftime(TIME_FORMAT)}</timeStamp>
-              <idContract>#{configuration.contract_id}</idContract>
-            </header>
-            <serviceData>
-              <ns2:getStats>
-                <ns2:dateBegin>#{from_date.strftime(TIME_FORMAT)}</ns2:dateBegin>
-                <ns2:dateEnd>#{to_date.strftime(TIME_FORMAT)}</ns2:dateEnd>
-              </ns2:getStats>
-            </serviceData>
-          </b2bRequest>
-        XML
+
+
+
+        doc = Ox::Document.new
+
+        instruct = Ox::Instruct.new(:xml)
+        instruct[:version] = '1.0'
+        instruct[:encoding] = 'UTF-8'
+        instruct[:standalone] = 'yes'
+        doc << instruct
+
+        b2b_request = Ox::Element.new('b2bRequest')
+        configuration.namespaces.each_pair { |ns, url| b2b_request[ns] = url }
+        doc << b2b_request
+
+        header = Ox::Element.new('header')
+
+        id_ext_transaction = Ox::Element.new('idExtTransaction')
+        id_ext_transaction << 1.to_s
+
+        time_stamp = Ox::Element.new('timeStamp')
+        time_stamp << Time.now.strftime(TIME_FORMAT)
+
+        id_contract = Ox::Element.new('idContract')
+        id_contract << configuration.contract_id
+
+        header << id_ext_transaction
+        header << time_stamp
+        header << id_contract
+        b2b_request << header
+
+
+        get_stats = Ox::Element.new('ns2:getStats')
+        date_begin = Ox::Element.new('ns2:dateBegin')
+        date_begin << from_date.strftime(TIME_FORMAT)
+        date_end = Ox::Element.new('ns2:dateEnd')
+        date_end << to_date.strftime(TIME_FORMAT)
+        get_stats << date_begin
+        get_stats << date_end
+
+        service_data = Ox::Element.new('serviceData')
+        service_data << get_stats
+        b2b_request << service_data
+
+        Ox.dump(doc)
       end
 
       private
