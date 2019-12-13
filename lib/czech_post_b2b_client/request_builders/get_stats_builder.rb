0d66e11eea3233128c94a5dd8e87c3ae9ab82510
Petr Mlčoch <foton@centrum.cz>
2019-12-13 15:56:43 +0100

    small refactor of GetStatsBuilder

--- lib/czech_post_b2b_client/request_builders/get_stats_builder.rb
+++ lib/czech_post_b2b_client/request_builders/get_stats_builder.rb
@@ -9,21 +9,36 @@ module CzechPostB2bClient
       end
 
       def to_xml
-
-
-
         doc = Ox::Document.new
+        doc << instruct
+        doc << b2b_request
 
+        Ox.dump(doc)
+      end
+
+      private
+
+      attr_reader :from_date, :to_date
+
+      def instruct
         instruct = Ox::Instruct.new(:xml)
         instruct[:version] = '1.0'
         instruct[:encoding] = 'UTF-8'
         instruct[:standalone] = 'yes'
-        doc << instruct
+        instruct
+      end
 
+      def b2b_request
         b2b_request = Ox::Element.new('b2bRequest')
         configuration.namespaces.each_pair { |ns, url| b2b_request[ns] = url }
-        doc << b2b_request
 
+        b2b_request << header
+        b2b_request << service_data
+
+        b2b_request
+      end
+
+      def header
         header = Ox::Element.new('header')
 
         id_ext_transaction = Ox::Element.new('idExtTransaction')
@@ -38,27 +53,28 @@ module CzechPostB2bClient
         header << id_ext_transaction
         header << time_stamp
         header << id_contract
-        b2b_request << header
 
-
-        get_stats = Ox::Element.new('ns2:getStats')
-        date_begin = Ox::Element.new('ns2:dateBegin')
-        date_begin << from_date.strftime(TIME_FORMAT)
-        date_end = Ox::Element.new('ns2:dateEnd')
-        date_end << to_date.strftime(TIME_FORMAT)
-        get_stats << date_begin
-        get_stats << date_end
-
-        service_data = Ox::Element.new('serviceData')
-        service_data << get_stats
-        b2b_request << service_data
-
-        Ox.dump(doc)
+        header
       end
 
-      private
 
-      attr_reader :from_date, :to_date
+      def service_data
+        service_data = Ox::Element.new('serviceData')
+
+        get_stats = Ox::Element.new('ns2:getStats')
+
+        date_begin = Ox::Element.new('ns2:dateBegin')
+        date_begin << from_date.strftime(TIME_FORMAT)
+
+        date_end = Ox::Element.new('ns2:dateEnd')
+        date_end << to_date.strftime(TIME_FORMAT)
+
+        get_stats << date_begin
+        get_stats << date_end
+        service_data << get_stats
+
+        service_data
+      end
     end
   end
 end
