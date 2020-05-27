5057c5977a3d621c796352039c053848cebca30b
Petr Mlčoch <foton@centrum.cz>
2020-05-27 10:48:25 +0200

    renamed service ParcelsSender to ParcelsAsyncSender

--- lib/czech_post_b2b_client/services.rb
+++ lib/czech_post_b2b_client/services.rb
@@ -3,8 +3,9 @@
 require 'czech_post_b2b_client/services/orchestrator'
 require 'czech_post_b2b_client/services/communicator'
 require 'czech_post_b2b_client/services/api_caller'
-require 'czech_post_b2b_client/services/parcels_sender'
+require 'czech_post_b2b_client/services/parcels_async_sender'
 require 'czech_post_b2b_client/services/parcels_immediate_sender'
+require 'czech_post_b2b_client/services/parcels_sender' # deprecated
 require 'czech_post_b2b_client/services/parcels_send_process_updater'
 require 'czech_post_b2b_client/services/address_sheets_generator'
 require 'czech_post_b2b_client/services/parcels_submission_closer'
--- lib/czech_post_b2b_client/services/parcels_sender.rb
+++ lib/czech_post_b2b_client/services/parcels_sender.rb
@@ -2,51 +2,10 @@
 
 module CzechPostB2bClient
   module Services
-    class ParcelsSender < CzechPostB2bClient::Services::Communicator
-      attr_reader :sending_data, :parcels
-
-      def initialize(sending_data:, parcels:)
-        @sending_data = sending_data
-        @parcels = parcels
-      end
-
-      private
-
-      def request_builder_args
-        { common_data: common_data, parcels: parcels }
-      end
-
-      def request_builder_class
-        CzechPostB2bClient::RequestBuilders::SendParcelsBuilder
-      end
-
-      def api_caller_class
-        CzechPostB2bClient::Services::ApiCaller
-      end
-
-      def response_parser_class
-        CzechPostB2bClient::ResponseParsers::SendParcelsParser
-      end
-
-      def common_data
-        data_from_config.merge(sending_data)
-      end
-
-      def data_from_config
-        {
-          contract_id: configuration.contract_id,
-          customer_id: configuration.customer_id,
-          sending_post_office_code: configuration.sending_post_office_code
-        }
-      end
-
-      def endpoint_path
-        '/sendParcels'
-      end
-
-      def build_result_from(response_hash)
-        OpenStruct.new(transaction_id: response_hash.dig(:async_result, :transaction_id),
-                       processing_end_expected_at: response_hash.dig(:async_result, :processing_end_expected_at))
+    class ParcelsSender < CzechPostB2bClient::Services::ParcelsAsyncSender
+      class << self
+        extend Gem::Deprecate
+        deprecate :call, 'CzechPostB2bClient::Services::ParcelsAsyncSender.call', 2020, 8
       end
     end
   end
